bb721f57844d9526dae5e8e0a10fed67bf2b0b20
VoduDay <andriy.maniak@gmail.com>
2020-11-13 18:02:33 +0200

    moved table to sepatate component

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,35 +1,12 @@
 <template>
-  <table class="contacts__table table">
-    <thead class="table__head">
-      <th>Name</th>
-      <th>Username</th>
-      <th>Email</th>
-      <th></th>
-    </thead>
-
-    <tbody class="table__body">
-      <tr v-if="contacts.length === 0">
-        <td colspan="4">You don't have any contacts</td>
-      </tr>
-      <tr
-        v-for="contact of contacts"
-        :key="contact.id"
-        class="table__contact"
-      >
-        <td>{{ contact.name }}</td>
-        <td>{{ contact.username }}</td>
-        <td><a :href="`mailto:${contact.email}`">{{ contact.email }}</a></td>
-        <td>
-          <router-link :to="{name: 'contact', params: {id: contact.id}}">Details</router-link>
-          <button type="button">X</button>
-          </td>
-      </tr>
-    </tbody>
-  </table>
+  <contacts-table
+    :contacts="contacts"
+  ></contacts-table>
 </template>
 
 <script>
 import store from '@/store';
+import ContactsTable from '@/components/ContactsTable.vue';
 
 export default {
   name: 'Contacts',
@@ -39,7 +16,7 @@ export default {
     };
   },
   components: {
-
+    ContactsTable,
   },
 };
 </script>
