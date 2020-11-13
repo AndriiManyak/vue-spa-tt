3634a8cf988ecd5742e8542d01749aa2735311e0
VoduDay <andriy.maniak@gmail.com>
2020-11-13 18:39:15 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
+    <Header />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+};
+</script>
 <style lang="scss">
 body {
   margin: 0;
@@ -15,5 +26,20 @@ body {
 
 a {
   color: #607D8B;
+  text-decoration: none;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+h1,
+h2,
+h3,
+h4,
+p {
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="header">
+    <nav>
+      <ul>
+        <li>
+          <router-link
+            to="/"
+          >
+            <h2>All contacts</h2>
+          </router-link>
+        </li>
+        <li>
+
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style>
+.header {
+  height: 60px;
+  padding: 0 20px;
+
+  color: white;
+  background-color: #143454;
+}
+</style>
