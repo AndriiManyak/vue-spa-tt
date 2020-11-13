605402844bdc7e61745bee544c56eeb3f44d9f35
VoduDay <andriy.maniak@gmail.com>
2020-11-13 16:43:24 +0200

    created basic views

---
+++ src/views/ContactDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="contactcard">
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Contacts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="contancs">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contacts',
+  data() {
+    return {
+      contacts: [],
+    };
+  },
+  components: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
