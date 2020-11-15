04f3125b26da9d12d944cb96bb11133441afd56e
VoduDay <andriy.maniak@gmail.com>
2020-11-15 21:56:01 +0200

    added form for adding fields

---
+++ src/components/NewFieldForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form class="new-field-form">
+    <input
+      type="text"
+      placeholder="Field name"
+    />
+    <input
+      type="text"
+      placeholder="Value"
+    />
+  </form>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
